0dd38de2f2a1685b6ee9cda56eee4e2fe074153b
impactlearner <time.darden@gmail.com>
2020-04-26 18:24:17 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,7 @@
 # Write your solution here
 current_time = Time.now
 current_time = current_time.to_i
-current_time.to_i % 2
-if true
+if current_time.to_i % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
